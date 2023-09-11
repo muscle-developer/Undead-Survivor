0cafab0ce01900ae7e2e20338ee32760844be212
Kimsinyoung <sinyoung5683@fireshrike.com>
2023-09-11 22:29:25 +0900

    셀 에니메이션 추가 (플레이러 달리기, 죽음, 기본 애니메이션)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,12 +11,15 @@ public class Player : MonoBehaviour
     [SerializeField]
     private Rigidbody2D playerRigidBody;
     [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
     private float playerSpeed = 5.0f;
 
     void Awake()
     {
         // Player의 RigidBody 컴포넌트를 선언한 변수에 넣어주기
         playerRigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer >();
     }
 
     // void Update()
@@ -34,6 +37,15 @@ public class Player : MonoBehaviour
         // 위치 이동 - 현재 나의 위치 + 내가 이동할 위치
         playerRigidBody.MovePosition(playerRigidBody.position + nextVector);
     }   
+    
+    // 프레임이 종료 되기 전 호출되는 함수
+    void LateUpdate()
+    {   
+        // 가로 x의 움직임이 - , + 가 될 떼 
+        // 현재 플레이어는 우측을 바라보기 때문에 - 값이 됐을 때만 flipX 를 true로 만들어준다.
+        if(inputVector.x != 0)
+            spriteRenderer.flipX = inputVector.x < 0;
+    }
 
     // Input System 에서 제공하는 이동함수
     private void OnMove(InputValue value)
