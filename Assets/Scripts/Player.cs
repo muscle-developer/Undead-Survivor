5bc41c1a1ce940fd48fc26ab18f960f3e806d4c3
Kimsinyoung <sinyoung5683@fireshrike.com>
2023-09-15 20:13:35 +0900

    애니메이터 스크립트 작성 및,  Animator Override Controller﻿ 생성

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,13 +13,16 @@ public class Player : MonoBehaviour
     [SerializeField]
     private SpriteRenderer spriteRenderer;
     [SerializeField]
+    private Animator playerAnimator;
+    [SerializeField]
     private float playerSpeed = 5.0f;
 
     void Awake()
     {
-        // Player의 RigidBody 컴포넌트를 선언한 변수에 넣어주기
+        // Player의 RigidBody 컴포넌트를 선언한 변수들 초기화
         playerRigidBody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer >();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // void Update()
@@ -41,6 +44,9 @@ public class Player : MonoBehaviour
     // 프레임이 종료 되기 전 호출되는 함수
     void LateUpdate()
     {   
+        // 움직이는 모션을 주기 위해 파라메터와 동일한 타입의 함수 호출, SetFloat("파라메터 이름", 반영할 값) , magnitude -> 벡터의 길이를 반환
+        playerAnimator.SetFloat("Speed", inputVector.magnitude);
+
         // 가로 x의 움직임이 - , + 가 될 떼 
         // 현재 플레이어는 우측을 바라보기 때문에 - 값이 됐을 때만 flipX 를 true로 만들어준다.
         if(inputVector.x != 0)
