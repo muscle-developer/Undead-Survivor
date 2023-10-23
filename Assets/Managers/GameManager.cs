40c904c9fc0f12e38b5eeb0ef9e4f9969f0971eb
Kimsinyoung <sinyoung5683@fireshrike.com>
2023-10-23 22:29:16 +0900

    몬스터 데이터 Class 생성 및 PlayTime에 따른 난이도 설정

--- Assets/Managers/GameManager.cs
+++ Assets/Managers/GameManager.cs
@@ -4,6 +4,12 @@ public class GameManager : MonoBehaviour
 {
     // 어디에서도 접근 가능 하도록 Static선언
     public static GameManager Instance;
+    // 내가 플레이한 시간
+    private float playTiem = 0f;
+    public float PlayTiem { get => playTiem; }
+    // 최대 플레이 할 수 있는 시간
+    [SerializeField]
+    private float maxPlayTime = 2 * 10f; 
     [SerializeField]
     private Transform managersObject;
     [SerializeField]
@@ -16,5 +22,13 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(managersObject);
         // Static 으로 선언한 변수는 인스펙터에 나타나지 않음으로 Awake에서 초기화 시켜준다.
         GameManager.Instance = this;
+    }
+
+    void Update()
+    {
+        playTiem += Time.deltaTime;
+
+        if(playTiem >= maxPlayTime)
+            playTiem = maxPlayTime;
     }
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,11 +1,16 @@
+using System;
 using UnityEngine;
 
+// 적의 스폰관련 클래스
 public class EnemySpawner : MonoBehaviour
 {  
     // 적이 생성될 위치(배열)
     public Transform[] enemySpawnPoint;
-    // 적  소환 타이머
-    public float timer = 0f;
+    public EnemySpawnData[] enemySpawnData;
+    // 적 소환 타이머
+    private float timer = 0f;
+    //난이도를 설정할 변수
+    private int level = 0;
 
     void Awake()
     {
@@ -17,10 +22,12 @@ public class EnemySpawner : MonoBehaviour
     {
         // 타이머에 시간을 계속 더해주자
         timer += Time.deltaTime;
-        // 일정 시간이 되면 소환이 되고 소환이 되면 다시 타이머를 0초로 돌려준다.
-        if(timer > 0.2f)
+        // 10초마다 1레벨이 오르게 설정
+        level =  Mathf.FloorToInt(GameManager.Instance.PlayTiem / 10f);
+        // Level이 0일때는 1초에 주기로 소환 Level이 1이상 부터는 0.5초 주기로 소환
+        if(timer > (level == 0 ? 1f : 0.5f))    
         {
-            EnemySpawn();
+            EnemySpawn(); 
             timer = 0f;
         }
     }
@@ -28,8 +35,24 @@ public class EnemySpawner : MonoBehaviour
     // 적을 소환하는 함수
     private void EnemySpawn()
     {
-        GameObject enemy = GameManager.Instance.poolManager.GetGameobject(Random.Range(1,4));
+        // 소환되는 타이밍도 레벨에 맞게 다른 몬스터가 생성되게 수정
+        GameObject enemy = GameManager.Instance.poolManager.GetGameobject(level);
         // 미리 만들어둔 SpawnPoint중 하나에 배치하자
-        enemy.transform.position = enemySpawnPoint[Random.Range(1, enemySpawnPoint.Length)].position;
+        enemy.transform.position = enemySpawnPoint[UnityEngine.Random.Range(1, enemySpawnPoint.Length)].position;
     }
+}
+
+// 직렬화[Serialization]:개체를 저장 혹은 전송하기 위해 변환, 즉 Inspector상에 보여주기 위해서 선언
+[Serializable]
+// 소환 데이터를 담당하는 클래스
+public class EnemySpawnData
+{
+    // 적 타입
+    public int enemyType = 0; 
+    // 적 소환 주기 
+    public float enemySpawnTime = 0f;
+    // 적의 체력
+    public float enemyHP = 0f;
+    // 적의 이동속도
+    public float enemySpeed = 0f;
 }
